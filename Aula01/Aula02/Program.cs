d49fc3a25fe6f7b1b59ec463c427da54b54e6100
RianLima2009 <113356176+RianLima2009@users.noreply.github.com>
2022-09-30 11:31:33 -0300

    Add files via upload

---
+++ Aula01/Aula02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Aula02
+{
+    class SegundaAula
+    {
+        static void Main(string[] args)
+        {
+            //int.parse
+
+            double numero1, numero2;
+            double resultado;
+          
+            
+
+            Console.WriteLine("Digite o primeiro número:");
+            numero1 = Convert.ToDouble(Console.ReadLine());
+         
+            Console.WriteLine("Digite o segundo número:");
+            numero2 = Convert.ToDouble(Console.ReadLine());
+
+            resultado = numero1 + numero2;
+            Console.WriteLine("Soma: " + resultado);
+            resultado = numero1 - numero2;
+            Console.WriteLine("Subtração: " + resultado);
+            resultado = numero1 * numero2;
+            Console.WriteLine("Multiplicação: " + resultado);
+            resultado = numero1 / numero2;
+            Console.WriteLine("Divisão: " + resultado);
+
+        }
+    }
+}
