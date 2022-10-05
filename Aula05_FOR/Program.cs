0abb2dfa69d0f4948fda1691bd97df19d071fa08
RianLima2009 <113356176+RianLima2009@users.noreply.github.com>
2022-10-05 11:29:23 -0300

    Add files via upload

---
+++ Aula05_FOR/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace Aula05_FOR//Estudo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            /*for(int i = 0; i<=10; i = i + 100)
+            {
+                Console.WriteLine("Contagem Crescente: "+i);
+            }*/
+
+           // Console.WriteLine("----------------------------------");
+            
+            for(int min=2; min>=0; min = min - 1)
+            {
+                for(int seg=60;seg>=0;seg=seg-1)
+               
+                Console.WriteLine("{0}:{1}",min,seg);     
+
+            }
+
+        }
+    }
+}
