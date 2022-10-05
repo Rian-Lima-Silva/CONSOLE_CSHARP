f5030ef57b2115ede7ffa9e46a354b5430391a20
RianLima2009 <113356176+RianLima2009@users.noreply.github.com>
2022-10-05 11:33:21 -0300

    Add files via upload

---
+++ Aula05_FOR_Desafio03/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace Aula05_FOR//Desafio_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for(int i = 101; i <= 200; i = i + 2)
+            {              
+                Console.WriteLine("Contagem: {0}", i);               
+            }
+        }
+    }
+}
---
+++ Aula05_FOR_Desafio04/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Aula05_FOR//Desafio04-Feito
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 1000; i = i + 1)
+            {
+                Console.WriteLine("Conragem Decrescente: {0}", i);
+            }
+        }
+    }
+}
