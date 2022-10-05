41c6dd79bf861bc39379a1409b095cf428a4e20b
RianLima2009 <113356176+RianLima2009@users.noreply.github.com>
2022-10-05 11:30:51 -0300

    Add files via upload

---
+++ Aula05_FOR_Desafio/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace Aula05_FOR//Desafio01-Feito
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 100; i >= 0; i=i-1)
+            {
+                Console.WriteLine("Conragem Decrescente: {0}",i);
+            }
+        }
+    }
+}
+
---
+++ Aula05_FOR_Desafio02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace Aula05_FOR//Desafio_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num,fatorial;
+            
+
+            Console.WriteLine("Qual o número que dev ser processado:");
+            num = int.Parse(Console.ReadLine());
+            
+            //5!=1*2*3*4*5
+            //5!=120
+
+            fatorial = num;
+            for (int i = num- 1; i >= 1; i=i-1)
+            {
+               
+
+                fatorial=fatorial * i;
+
+     
+
+            }
+
+
+            Console.WriteLine("{0}!={1}",num,fatorial);
+
+        }
+    }
+}
