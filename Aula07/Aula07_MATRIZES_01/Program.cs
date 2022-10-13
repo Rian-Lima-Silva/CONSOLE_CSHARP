5d8ce0eef81113eaa60fe575701707731af48f0e
RianLima2009 <113356176+RianLima2009@users.noreply.github.com>
2022-10-13 08:12:32 -0300

    Add files via upload

---
+++ Aula07/Aula07_MATRIZES_01/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Aula07_MATRIZES_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Vetores: [POSIÇÃO]
+             Matriz: [Linha e Coluna] [1,2]=Uma Linha e duas colunas
+            Ex:
+            Cadastro de 1 pessoa:Dados->Nome,Email,Senha
+            Matriz[1,3] ou Matriz[1 Linha e 3 Colunas]
+            
+             Cadastro de 2 pessoas:Dados->Nome,Email,Senha
+            Matriz [2,3] ou Matriz [2 Linha e 3 Colunas]*/
+
+
+            string[,] n = new string[3, 3];
+
+         
+               n[0, 0] = "Nome";
+               n[0, 1] = "Sobrenome";
+               n[0, 2] = "Final";
+             
+               n[1, 0] = "";
+               n[1, 1] = ""; 
+               n[1, 2] = "";
+
+               n[2, 0] = "";
+               n[2, 1] = "";
+               n[2, 2] = "";
+              
+
+            for (int i = 0; i <3; i++)
+            {
+                for (int a = 0; a < 3; a++)
+                {
+                   
+                        Console.WriteLine("Digite o seu NOME:");
+                        n[i, a] = Console.ReadLine();
+                }
+
+            }
+
+            for (int i = 0; i < 3; i++)
+            {               
+                    Console.WriteLine("{0}º {1} {2} {3}", i, n[i, 0], n[i, 1], n[i, 2]);
+            }
+            
+            
+
+        }
+    }
+}
---
+++ Aula07/Aula07_VETORES_01/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Aula07_VETORES_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[] n = new int[4] {0,1,2,3};
+            string[] L = new string[4] {"Nome","Um","Dois","Três"};
+
+
+             for (int i = 0; i < 4; i++)
+             {
+                Console.WriteLine("Contando: {0}|{1}", n[i],L[i]);
+                             
+             }
+
+            //string[,] L = new string[2,3];
+
+            /*Ao colocar no FOR é melhor usar a Linha com index, pois melhora a impressão de informações
+             EX:Cadastro de 2 pessoa:Dados->Nome,Email,Senha
+                                |   | 0 | 1 | 2 |
+                n[0,0]=nome     | 0 |   |   |   |
+                n[0,1]=email    | 1 |   |   |   |
+                n[0,2]=senha    |---------------|
+                                
+                                
+                Matriz[2,3] ou Matriz[2 Linha e 3 Colunas]
+                Nessa Matriz e melhor usar o nome da pessoa como index
+                n[i,0] = n[Linha(i),e coluna 0)
+                Console.WriteLine(n[i,0],n[i,1],n[i,2]); == Escreva n[index=0,coluna=0]
+            
+            
+             
+             */
+                /*for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine("{0}\n{1}\n{2}\n",n[i,0], n[i,1], n[i,2]);
+
+
+                }*/
+
+
+}
+}
+}
